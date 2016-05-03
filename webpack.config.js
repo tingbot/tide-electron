d23629cbee727e963a6cc321868b52340fa733f6
Robert Wilson <robert.wilson1717@gmail.com>
2016-05-03 18:33:43 +0100

    Fix webpacking to dist folder and add missing deps to npm config

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,8 @@ var webpack = require('webpack')
 module.exports = {
     entry: ['font-awesome-webpack!./font-awesome.config.js','./app/main.js'],
     output: {
-        path: './',
+        path: './dist',
+        publicPath: '/dist/',
         filename: 'build.js'
     },
     target: 'electron',
@@ -18,10 +19,10 @@ module.exports = {
             exclude: /node_modules/
         }, {
             test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-            loader: "url-loader?limit=10000&minetype=application/font-woff&name=./dist/[hash].[ext]"
+            loader: "url-loader?limit=10000&minetype=application/font-woff"
         }, {
             test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-            loader: "file-loader?name=./dist/[name].[ext]"
+            loader: "file-loader"
         }]
     },
     babel: {
