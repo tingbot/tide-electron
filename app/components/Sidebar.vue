3b7c0debbcf8b919acb1255dce53587ea61d993b
Joe Rickerby <joerick@mac.com>
2016-05-09 17:24:50 +0100

    Always put the main or main.py file at the top of the sidebar

--- app/components/Sidebar.vue
+++ app/components/Sidebar.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="left" id="sidebar">
-    <template v-for="file in root.files">
+    <template v-for="file in sortedFiles">
       <file :file="file"></file>
     </template>
   </div>
@@ -11,6 +11,22 @@
 
   export default {
     props: ['root'],
-    components: [File]
+    components: [File],
+    computed: {
+      sortedFiles: function () {
+        // we want the main or main.py file to be at the top of the list.
+        // Otherwise, alphabetical.
+        let result = this.root.files.slice();
+        return result.sort((a, b) =>  {
+          if (a.name === 'main.py' || a.name === 'main') {
+            return -1;
+          } else if (b.name === 'main.py' || b.name === 'main') {
+            return 1;
+          } else {
+            return a.name.localeCompare(b.name);
+          }
+        });
+      }
+    }
   }
 </script>
