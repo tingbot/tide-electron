c2dbfc9a2c5fa17361cbab87aa3039f4aaaba401
Joe Rickerby (windows) <joerick@mac.com>
2016-08-31 18:02:17 +0100

    Implement autoupdate on Windows

--- app/autoupdate.js
+++ app/autoupdate.js
@@ -8,34 +8,38 @@ const os = require('os');
 console.assert(process.type !== 'renderer');
 
 function showAutoupdateDialog(releaseName, callback) {
-	dialog.showMessageBox({
-		type: 'question',
-		title: 'Update available',
-		message: 'An update to Tide is available',
-		detail: 'The latest version is '+releaseName+'. You are using '+app.getVersion()+'.',
-		buttons: ['Update', 'Cancel'],
-	}, function (buttonId) {
-		if (buttonId == 0) {
-			callback();
-		}
-	});
+    dialog.showMessageBox({
+        type: 'question',
+        title: 'Update available',
+        message: 'An update to Tide is available',
+        detail: 'The latest version is '+releaseName+'. You are using '+app.getVersion()+'.',
+        buttons: ['Update', 'Cancel'],
+    }, function (buttonId) {
+        if (buttonId == 0) {
+            callback();
+        }
+    });
 };
 
 module.exports.setup = function () {
-	if (process.platform === 'darwin') {
-		const platform = os.platform() + '_' + os.arch();
-		const version = app.getVersion();
+    autoUpdater.on('error', function (err) { console.log('Autoupdate: error', err); })
+    autoUpdater.on('update-available', function () { console.log('Autoupdate: update available, downloading...')})
+    autoUpdater.on('update-not-available', function () { console.log('Autoupdate: Up to date.')})
+    autoUpdater.on('update-downloaded', function (event, releaseNotes, releaseName) {
+        showAutoupdateDialog(releaseName, function () {
+            autoUpdater.quitAndInstall();
+        });
+    })
 
-		autoUpdater.on('error', function (err) { console.log('Autoupdate: error', err); })
-		autoUpdater.on('update-available', function () { console.log('Autoupdate: update available, downloading...')})
-		autoUpdater.on('update-not-available', function () { console.log('Autoupdate: Up to date.')})
-		autoUpdater.on('update-downloaded', function (event, releaseNotes, releaseName) {
-			showAutoupdateDialog(releaseName, function () {
-				autoUpdater.quitAndInstall();
-			});
-		})
+    const version = app.getVersion();
 
-		autoUpdater.setFeedURL('http://tide-download.tingbot.com/update/'+platform+'/'+version);
-		autoUpdater.checkForUpdates();
+    if (process.platform === 'darwin') {
+        const platform = os.platform() + '_' + os.arch();
+
+        autoUpdater.setFeedURL('http://tide-download.tingbot.com/update/'+platform+'/'+version);
+        autoUpdater.checkForUpdates();
+    } else if (process.platform === 'win32') {
+        autoUpdater.setFeedURL('http://tide-download.tingbot.com/update/win32/'+version);
+        autoUpdater.checkForUpdates();
     }
 }
